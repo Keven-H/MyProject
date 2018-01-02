c22fad0fb2a88e2512182576baebe9f0f8054c5f
Keven-H <huanjianzhong@Fengnian.cn>
2018-01-02 19:06:50 +0800

    创建project

---
+++ MyProject/MyProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyProject
+//
+//  Created by Admin on 2018/1/2.
+//  Copyright © 2018年 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
